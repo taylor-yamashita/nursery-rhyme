5da9fd98990df6ffd3d7aee86343210da14f4b95
Taylor Yamashita <taylory@princeton.edu>
2022-08-03 14:14:26 -0700

    create test to check if class returns rhyme string

---
+++ spec/rhyme_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require_relative '../rhyme.rb'
+
+describe JacksHouse do
+    subject(:jack) {JacksHouse.new}
+
+    it 'returns nursery rhyme string' do 
+        rhyme = File.read("rhyme.txt")
+        expect(jack.rhyme).to eq(rhyme)
+    end
+end
