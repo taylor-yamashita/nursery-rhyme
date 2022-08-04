215ff1bf44a23e1615d9dd01fa3e320a02464e5f
Taylor Yamashita <taylory@princeton.edu>
2022-08-04 09:47:41 -0700

    add method to create randomized rhyme

--- rhyme.rb
+++ rhyme.rb
@@ -1,27 +1,19 @@
 class JacksHouse
   # generate normal nursery rhyme
   def normal_rhyme
-    rhyme = []
-    (0..(phrases.length - 1)).each do |i|
-      line = ["This is"]
-      j = i
-      while j >= 0
-        line << phrases[j]
-        j -= 1
-      end
-      rhyme << line.join(" ") + "."
-    end
-    rhyme.join("\n")
+    construct_rhyme(base_phrases)
   end
 
   # generate randomized nursery rhyme
   def random_rhyme
+    random_phrases = base_phrases.shuffle
+    construct_rhyme(random_phrases)
   end
 
   private
 
   # nursery rhyme components
-  def phrases
+  def base_phrases
     ["the house that Jack built",
       "the malt that lay in",
       "the rat that ate",
@@ -35,4 +27,22 @@ class JacksHouse
       "the farmer sowing his corn that kept",
       "the horse and the hound and the horn that belonged to"]
   end
+
+  def construct_rhyme(phrases)
+    rhyme = []
+    (0..(phrases.length - 1)).each do |i|
+      line = ["This is"]
+      j = i
+      while j >= 0
+        line << phrases[j]
+        j -= 1
+      end
+      rhyme << line.join(" ") + "."
+    end
+    rhyme.join("\n")
+  end
 end
+
+
+j = JacksHouse.new
+puts j.random_rhyme
