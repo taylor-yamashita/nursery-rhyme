c838b668a71d3d6cf9219b29660055409ea52490
Taylor Yamashita <taylory@princeton.edu>
2022-08-04 09:31:15 -0700

    styling with standardrb

--- rhyme.rb
+++ rhyme.rb
@@ -1,34 +1,34 @@
-class JacksHouse    
-    # generate nursery rhyme
-    def generate_rhyme
-        rhyme = []
-        for i in 0..(phrases.length()-1)
-            line = ["This is"]
-            j = i
-            while j >= 0
-                line << phrases[j]
-                j -= 1
-            end
-            rhyme << line.join(" ") + "."
-        end
-        return rhyme.join("\n")
+class JacksHouse
+  # generate nursery rhyme
+  def generate_rhyme
+    rhyme = []
+    (0..(phrases.length - 1)).each do |i|
+      line = ["This is"]
+      j = i
+      while j >= 0
+        line << phrases[j]
+        j -= 1
+      end
+      rhyme << line.join(" ") + "."
     end
+    rhyme.join("\n")
+  end
 
-    private
+  private
 
-    # nursery rhyme components
-    def phrases
-        ["the house that Jack built",
-        "the malt that lay in",
-        "the rat that ate",
-        "the cat that killed",
-        "the dog that worried",
-        "the cow with the crumpled horn that tossed",
-        "the maiden all forlorn that milked",
-        "the man all tattered and torn that kissed",
-        "the priest all shaven and shorn that married",
-        "the rooster that crowed in the morn that woke",
-        "the farmer sowing his corn that kept",
-        "the horse and the hound and the horn that belonged to"]
-    end
+  # nursery rhyme components
+  def phrases
+    ["the house that Jack built",
+      "the malt that lay in",
+      "the rat that ate",
+      "the cat that killed",
+      "the dog that worried",
+      "the cow with the crumpled horn that tossed",
+      "the maiden all forlorn that milked",
+      "the man all tattered and torn that kissed",
+      "the priest all shaven and shorn that married",
+      "the rooster that crowed in the morn that woke",
+      "the farmer sowing his corn that kept",
+      "the horse and the hound and the horn that belonged to"]
+  end
 end
