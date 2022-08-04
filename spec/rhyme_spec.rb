b60ff94e845ad48c133a717d0c3094aefd41e1c6
Taylor Yamashita <taylory@princeton.edu>
2022-08-04 10:43:58 -0700

    add test to check that two random rhymes are not equal

--- spec/rhyme_spec.rb
+++ spec/rhyme_spec.rb
@@ -4,12 +4,14 @@ require_relative "../rhyme"
 describe JacksHouse do
   subject(:jack) { JacksHouse.new }
 
-  it "returns a normal nursery rhyme string" do
+  it "generates a normal nursery rhyme" do
     rhyme = File.read("rhyme.txt")
     expect(jack.normal_rhyme).to eq(rhyme)
   end
 
-  # it "returns a randomized nursery rhyme string" do
-  #   expect(jack.random_rhyme).to eq()
-  # end
+  it "generates a randomized nursery rhyme" do
+    random_rhyme_1 = jack.random_rhyme
+    random_rhyme_2 = jack.random_rhyme
+    expect(random_rhyme_1).not_to eq(random_rhyme_2)
+  end
 end
